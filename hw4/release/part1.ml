76c96f22faee4683b4c5345ddbc0b9852f8c8b11
Jeff Mu <smartmu@gmail.com>
2012-03-10 20:06:06 -0500

    started working on part1

--- hw4/release/part1.ml
+++ hw4/release/part1.ml
@@ -17,8 +17,27 @@ type graph = int * weighted_edge list
 (* Kruskal's Algorithm takes a number of nodes and a list of
  * edges representing an undirected graph and returns the
  * edges which make up the minimal spanning tree (or forest) *)
-let kruskal (g : graph) : (weighted_edge list)= 
-  failwith "not implemented"
+let kruskal (g : graph) : (weighted_edge list) = 
+  let n,l = g in
+  let compare_edge e1 e2 = 
+    let _,_,w1 = e1
+    and _,_,w2 = e2 in
+    compare w1 w2 in
+  let sorted_edges = List.rev (List.sort compare_edge l) in
+  let rec connected lst e = 
+    match lst with
+      | [] -> false
+      | h::t -> let (v1,v2,_) = h 
+                and (n1,n2,_) = e in
+                if v1=n1 && v2=n2 then true
+                if v1=n1 then connected t 
+                else connected t v1 n1 || connected t v2 n2 in
+  let rec helper edges mst = 
+    match edges with
+      | [] -> mst
+      | h::t -> if connected mst h then helper t mst
+                else helper t (h::mst) in
+  (n, helper sorted_edges [])
     
 (* List of contiguous integers: 1 <|> 5 = [1; 2; 3; 4; 5] *)
 let (<|>) x y =

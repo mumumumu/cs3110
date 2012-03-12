492912c62eb97121722eabb00f51cfa63ec984e4
Jeff Mu <smartmu@gmail.com>
2012-03-12 17:00:22 -0400

    finished part1

--- hw4/release/part1.ml
+++ hw4/release/part1.ml
@@ -33,14 +33,18 @@ let kruskal (g : graph) : (weighted_edge list) =
 								let t1 = find u n1 
 								and t2 = find u n2 in
 								if t1 = t2 then helper t mst
-								else let _ = union u n1 n2 in helper t (h::mst) in
+								else (union u n1 n2 ; helper t (h::mst)) in
   helper sorted_edges []
 
-let kruskal_test =
-  kruskal (2,[(0,1,2.0);(1,0,3.0)]);
-  kruskal (3,[(0,1,1.0);(1,2,1.0);(2,0,3.0)]);
-  kruskal (5,[(0,1,1.0);(1,2,1.0);(2,3,1.0);(3,4,1.0);(4,0,1.0);(1,3,1.0);
-							(1,4,1.0);(2,4,1.0);(2,0,1.0);(3,0,1.0)])
+(*
+let kt0 = kruskal (0,[]) = []
+let kt1 = kruskal (1,[0,0,max_float]) = []
+let kt2 = kruskal (2,[(0,1,max_float);(1,0,min_float)]) = [(0,1,max_float)]
+let kt3 = kruskal (2,[(0,1,min_float);(1,0,min_float)])
+let kt4 = kruskal (3,[(0,1,0.0);(1,2,0.0);(2,0,0.0)]) 
+let kt5 = kruskal (5,[(0,1,0.5);(1,2,1.0);(2,3,0.5);(3,4,1.0);(4,0,1.0);
+                      (1,3,0.5);(1,4,0.5);(2,4,1.0);(2,0,0.5);(3,0,1.0)])
+*)
 
 (* List of contiguous integers: 1 <|> 5 = [1; 2; 3; 4; 5] *)
 let (<|>) x y =
@@ -51,79 +55,40 @@ let (<|>) x y =
  * algorithm described in the writeup.
  * Requires: n > 2 *)
 let calc_p_threshold (n:int) : float =
+  Random.self_init ();
   let s = n*n in
-  let u = createUniverse s in
-  let u2 = createUniverse s in
-  let _ = Random.self_init in
-  let top = (<|>) 0 (n-1)
-  and bot = (<|>) (s-n) (s-1) 
-  and occupied = (<|>) n (s-n-1) in
-  let occupied_array = Array.of_list occupied in
-  let swap a i j = let t = a.(i) in a.(i) <- a.(j); a.(j) <- t in
-  let shuffle a = Array.iteri (fun i _ -> swap a i (Random.int (i+1))) a in
-  (*let shuffle = List.sort (fun a b -> (Random.int 3) - 1) in *)
-  let _ = shuffle occupied_array in
-  let occupied_list = Array.to_list occupied_array in
-  let _ = List.map (union u 0) top in
-  let _ = List.map (union u (s-1)) bot in
-  let _ = List.map (union u2 0) top in
-  let _ = List.map (union u2 0) bot in
+  let u = createUniverse s and u2 = createUniverse s in
+  let top = (<|>) 0 (n-1) and bot = (<|>) (s-n) (s-1) 
+  (*note:List.sort (fun _ _ -> (Random.int 3) - 1) is not a uniform shuffle*)
+  (*credz to http://www.codecodex.com/wiki/index.php?title=Shuffle_an_array#OCaml_:_Array.sort*)
+  and occupied = 
+    let occupied_array = Array.of_list ((<|>) n (s-n-1)) in
+    let swap a i j = let t = a.(i) in a.(i) <- a.(j); a.(j) <- t in
+    let shuffle a = Array.iteri (fun i _ -> swap a i (Random.int (i+1))) a in
+    shuffle occupied_array;
+    Array.to_list occupied_array in
+  ignore (List.map (union u 0) top) ; ignore (List.map (union u (s-1)) bot) ;
+  ignore (List.map (union u2 0) top) ; ignore (List.map (union u2 0) bot) ;
   let make_vacant i = 
-    let up = find u (i-n)
-    and dn = find u (i+n) 
+    let up = find u (i-n) and dn = find u (i+n) 
     and lt = find u (i-(if (i mod n) = 0 then 0 else 1)) 
     and rt = find u (i+(if (i mod n) = (n-1) then 0 else 1)) in
-    (*let _ = print_string ((string_of_int t) ^ " " ^ (string_of_int b) ^ " " ^
-    (string_of_int up) ^ " " ^ (string_of_int dn) ^ " " ^ (string_of_int lt) ^ " " ^
+    (*let _ = print_string ((string_of_int up) ^ " " ^ (string_of_int dn) ^ " " ^ (string_of_int lt) ^ " " ^
     (string_of_int rt) ^ "\n") in*)
-		let _ = if ((find u2 up) = 0) then union u up i in
-		let _ = if ((find u2 dn) = 0) then union u dn i in
-		let _ = if ((find u2 lt) = 0) then union u lt i in
-		let _ = if ((find u2 rt) = 0) then union u rt i in
+		if ((find u2 up) = (find u2 0)) then union u up i ;
+		if ((find u2 dn) = (find u2 0)) then union u dn i ;
+		if ((find u2 lt) = (find u2 0)) then union u lt i ;
+		if ((find u2 rt) = (find u2 0)) then union u rt i ;
     () in
 	let rec helper l = 
-    if ((find u 0) = (find u (s-1))) then float_of_int(size u 0)/.float_of_int(s)
-    else let h::t = l in
-			   (*let _ = print_string ((string_of_int h) ^ "\n") in*)
- 				 let _ = union u2 0 h in
-         let _ = make_vacant h in
-         helper (*((Random.int (s-(2*n)-1))+n)*) t in
-  helper (*((Random.int (s-(2*n)-1)+n))*)occupied_list
-  
-  let calc_p_threshold2 (n:int) : float =
-  let s = n*n in
-  let u = createUniverse s in
-  let u2 = createUniverse s in
-  let _ = Random.self_init in
-  let top = (<|>) 0 (n-1)
-  and bot = (<|>) (s-n) (s-1) 
-  and occupied = (<|>) n (s-n-1) in
-  let shuffle = List.sort (fun a b -> (Random.int 3) - 1) in 
-  let _ = List.map (union u 0) top in
-  let _ = List.map (union u (s-1)) bot in
-  let _ = List.map (union u2 0) top in
-  let _ = List.map (union u2 0) bot in
-  let make_vacant i = 
-    let up = find u (i-n)
-    and dn = find u (i+n) 
-    and lt = find u (i-(if (i mod n) = 0 then 0 else 1)) 
-    and rt = find u (i+(if (i mod n) = (n-1) then 0 else 1)) in
-    (*let _ = print_string ((string_of_int t) ^ " " ^ (string_of_int b) ^ " " ^
-    (string_of_int up) ^ " " ^ (string_of_int dn) ^ " " ^ (string_of_int lt) ^ " " ^
-    (string_of_int rt) ^ "\n") in*)
-		let _ = if ((find u2 up) = 0) then union u up i in
-		let _ = if ((find u2 dn) = 0) then union u dn i in
-		let _ = if ((find u2 lt) = 0) then union u lt i in
-		let _ = if ((find u2 rt) = 0) then union u rt i in
-    () in
-	let rec helper l = 
-    if ((find u 0) = (find u (s-1))) then float_of_int(size u 0)/.float_of_int(s)
-    else let h::t = l in 
-			   (*let _ = print_string ((string_of_int h) ^ "\n") in*)
- 				 let _ = union u2 0 h in
-         let _ = make_vacant h in
-         helper (*(shuffle*) t(*)*) in
-  helper (shuffle occupied)
+    if ((find u 0) = (find u (s-1))) then float_of_int(size u2 0)/.float_of_int(s)
+    else match l with 
+          | h::t -> (*let _ = print_string ((string_of_int h) ^ "\n") in*)
+                    union u2 0 h ;
+                    make_vacant h ;
+                    helper t
+          | [] -> failwith ("Failure: grid vacant, but no percolation found")
+  in helper occupied
 
 (* Averages the percolation threshold for an nxn grid
  * over i trials *)  

98f1be36f1febeec37356c073c8f25a4e4e19f0c
Jeff Mu <smartmu@gmail.com>
2012-03-18 00:44:00 -0400

    comments

--- hw4/release/segmentation.ml
+++ hw4/release/segmentation.ml
@@ -26,6 +26,7 @@ let sort_edges edges =
 let segmentGraph ((n, wedges) : graph) (k : float) : segmentation =
   let s = D.createUniverse n in
   let t_c v = k/.(float_of_int v) in
+  (* initial array of threshold values for each 1-node component *)
   let threshold = Array.make n (t_c 1) in
   let rec helper e_lst =
     match e_lst with 
@@ -36,6 +37,7 @@ let segmentGraph ((n, wedges) : graph) (k : float) : segmentation =
 					let _ = if (a != b) && (w <= (min threshold.(a) threshold.(b))) then
             let _ = D.union s a b in
             let q = D.find s a in
+            (* update threshold value for root node *)
             threshold.(q) <- (w +. (t_c (D.size s q))); () in
           helper t
   in helper (sort_edges wedges)
@@ -53,18 +55,6 @@ let mergeSmallComponents (u : segmentation) (edges : weighted_edge list)
           helper t
   in helper (sort_edges edges)
   
-let components ((n, _) : graph) (s : segmentation) : int list list =
-  let comp = Array.make n [] in
-  let rec helper n = 
-    if n >= 0 then 
-      let i = D.find s (n) in
-      let _ = comp.(i) <- ((n+1)::(comp.(i))) in
-      helper (n-1) 
-    else 
-			List.fold_left
-				(fun l x -> if x != [] then x::l else l) [] (Array.to_list comp)
-  in helper (n-1)
-  
 let segmentImage (img : I.image) (sigma : float) (k : float)
 		(minSize : int) : segmentation =
   let simg = I.smooth sigma img in
@@ -73,6 +63,7 @@ let segmentImage (img : I.image) (sigma : float) (k : float)
     let r1,g1,b1 = (I.getPixel simg x1 y1) 
     and r2,g2,b2 = (I.getPixel simg x2 y2) in
     sqrt (((r1-.r2)**2.0) +. ((g1-.g2)**2.0) +. ((b1-.b2)**2.0)) in
+  (* create list of edges by folding across the image *)
   let edges = I.fold (fun x y c l ->
     let l1 = if x<w-1 then (y*w+x,y*w+x+1,diff x y (x+1) y)::l 
 			else l in
@@ -97,14 +88,18 @@ let segmentImage (img : I.image) (sigma : float) (k : float)
 let colorComponents (img : I.image) (u : segmentation) : I.image =
   let w = I.width img and h = I.height img in
   let colors = Array.make (w*h) (0.0,0.0,0.0) in
+  (* sum up all color values in each component *)
 	let _ = I.fold (fun x y c _ -> 
                     let i = D.find u (w*y+x) in
     								let r1,g1,b1 = colors.(i) and r2,g2,b2 = c in
     								colors.(i) <- (r1+.r2,g1+.g2,b1+.b2)) () img in
+  (* average color values for each component *)
 	let avg_colors = Array.mapi 
 			(fun i a -> let r,g,b = a and n = float_of_int (D.size u i) in
     	(r/.n,g/.n,b/.n))	colors in
+  (* create new image with new colors *)
 	let new_img = I.map (fun x y c -> avg_colors.(D.find u (w*y+x))) img in
+  (* add white border between components *)
   I.map (fun x y c -> let n = D.find u (w*y+x) in
            let up = D.find u (if y<h-1 then (w*(y+1)+x) else n)
            and dn = D.find u (if y>0 then (w*(y-1)+x) else n)
@@ -117,14 +112,26 @@ let colorComponents (img : I.image) (u : segmentation) : I.image =
     if n != up || n != dn || n != rt || n != lt || 
        n != dr || n != dl || n != ur || n != ul then (255.0,255.0,255.0) 
     else c) new_img
-    
+
+let components ((n, _) : graph) (s : segmentation) : int list list =
+  let comp = Array.make n [] in
+  let rec helper n = 
+    (* create an array of lists, adding nodes to the root node they belong to *)
+    if n >= 0 then 
+      let i = D.find s (n) in
+      let _ = comp.(i) <- ((n+1)::(comp.(i))) in
+      helper (n-1) 
+    else 
+			List.fold_left
+				(fun l x -> if x != [] then x::l else l) [] (Array.to_list comp)
+  in helper (n-1)
+      
 (* Segments the image stored in the bitmap file located at img_filename
  * with the given parameters. Saves and shows the results *)
 let segmentAndShow (img_filename : string) (sigma : float)
       (k : float) (min_size : int) : unit =
   let img = I.load img_filename in
   let seg = segmentImage img sigma k min_size in
-  let _ = print_int (List.length (components (154401,[]) seg)) in
   let colored = colorComponents img seg in
     I.save colored "output/segmented.bmp";
     I.show colored

51b02d32c233747bfaa9e3a654a08dd3de12bba5
Jeff Mu <smartmu@gmail.com>
2012-03-18 00:36:09 -0400

    segmentation works - used n instead of 1 in the beginning of the algorithm - doh; todo: comments

--- hw4/release/segmentation.ml
+++ hw4/release/segmentation.ml
@@ -13,30 +13,32 @@ type weighted_edge = int * int * float
 type graph = int * weighted_edge list
 
 (* Represents a partition of the graph into connected components *)
-(* TODO: Define this! *)
-type segmentation = disjointSet
+type segmentation = DisjointSet.universe
+
+(* sorts a list of edges in increasing order by weight*)
+let sort_edges edges = 
+  let cmp e1 e2 = let _,_,w1 = e1 and _,_,w2 = e2 in compare w1 w2 in
+  List.sort cmp edges
 
 (* segmentGraph g k segments g using the parameter
  * k for the threshold function
  * Returns: a segmentation of the graph *)
 let segmentGraph ((n, wedges) : graph) (k : float) : segmentation =
   let s = D.createUniverse n in
-  let sort_edges = 
-    let cmp e1 e2 = let _,_,w1 = e1 and _,_,w2 = e2 in compare w1 w2 in
-    List.sort cmp wedges in
   let t_c v = k/.(float_of_int v) in
-  let threshold = Array.make n (t_c n) in
+  let threshold = Array.make n (t_c 1) in
   let rec helper e_lst =
     match e_lst with 
       | [] -> s
-      | h::t -> let n1,n2,w = h in
-                let a = D.find s n1 and b = D.find s n2 in
-                let _ = 
-									if a != b && w <= thershold.(a) && w <= threshold.(b) then
-                  	D.union s a b;
-                  	threshold.(a) <- w + t_c(D.size u a) in
-                helper t
-  in helper sort_edges
+      | h::t -> 
+          let n1,n2,w = h in
+          let a = (D.find s n1) and b = (D.find s n2) in
+					let _ = if (a != b) && (w <= (min threshold.(a) threshold.(b))) then
+            let _ = D.union s a b in
+            let q = D.find s a in
+            threshold.(q) <- (w +. (t_c (D.size s q))); () in
+          helper t
+  in helper (sort_edges wedges)
 
 let mergeSmallComponents (u : segmentation) (edges : weighted_edge list)
 		(minSize : int) : segmentation =
@@ -45,20 +47,32 @@ let mergeSmallComponents (u : segmentation) (edges : weighted_edge list)
       | [] -> u 
       | h::t -> 
 					let n1,n2,w = h in
-          let a = D.find s n1 and let b = D.find s n2 in
-          if ((a != b) && ((D.size s a < minSize) || (D.size s b < min_size))) then
-          	D.union s a b;
+          let a = (D.find u n1) and b = (D.find u n2) in
+          if (a != b) && ((D.size u a) < minSize || (D.size u b) < minSize) then
+          	D.union u a b;
           helper t
-  in helper edges
-
+  in helper (sort_edges edges)
+  
+let components ((n, _) : graph) (s : segmentation) : int list list =
+  let comp = Array.make n [] in
+  let rec helper n = 
+    if n >= 0 then 
+      let i = D.find s (n) in
+      let _ = comp.(i) <- ((n+1)::(comp.(i))) in
+      helper (n-1) 
+    else 
+			List.fold_left
+				(fun l x -> if x != [] then x::l else l) [] (Array.to_list comp)
+  in helper (n-1)
+  
 let segmentImage (img : I.image) (sigma : float) (k : float)
 		(minSize : int) : segmentation =
   let simg = I.smooth sigma img in
-  let w = width simg and h = height simg in
+  let w = I.width simg and h = I.height simg in
   let diff x1 y1 x2 y2 = 
-    let r1,g1,b1 = (getPixel simg x1 y1) 
-		and r2,g2,b2 = (getPixel simg x2 y2) in
-    sqrt (((r1-r2)**2) + ((g1-g2)**2) + ((b1-b2)**2)) in
+    let r1,g1,b1 = (I.getPixel simg x1 y1) 
+    and r2,g2,b2 = (I.getPixel simg x2 y2) in
+    sqrt (((r1-.r2)**2.0) +. ((g1-.g2)**2.0) +. ((b1-.b2)**2.0)) in
   let edges = I.fold (fun x y c l ->
     let l1 = if x<w-1 then (y*w+x,y*w+x+1,diff x y (x+1) y)::l 
 			else l in
@@ -68,7 +82,7 @@ let segmentImage (img : I.image) (sigma : float) (k : float)
 			else l2 in
     let l4 = if y>0 then (y*w+x,(y-1)*w+x,diff x y x (y-1))::l3 
 			else l3 in
-    let l5 = if x<w-1 && y<h-1 then (y*w+x,(y+1)*w+x+1,diff x y (x+1) (y+1))::l4 
+    let l5 = if x<w-1 && y<h-1 then (y*w+x,(y+1)*w+x+1,diff x y (x+1) (y+1))::l4
 			else l4 in
     let l6 = if x>0 && y<h-1 then (y*w+x,(y+1)*w+x-1,diff x y (x-1) (y+1))::l5
 			else l5 in
@@ -81,35 +95,38 @@ let segmentImage (img : I.image) (sigma : float) (k : float)
   mergeSmallComponents seg edges minSize
   
 let colorComponents (img : I.image) (u : segmentation) : I.image =
-  let w = width img and h = height img in
+  let w = I.width img and h = I.height img in
   let colors = Array.make (w*h) (0.0,0.0,0.0) in
-	let _ = I.fold (fun x y c _ -> let i = D.find u (w*y+x) in
-    														 let r1,g1,b1 = colors.(i) and r2,g2,b2 = c in
-    														 colors.(i) <- (r1+r2,g1+g2,b1+b2))
-		  _ img in
+	let _ = I.fold (fun x y c _ -> 
+                    let i = D.find u (w*y+x) in
+    								let r1,g1,b1 = colors.(i) and r2,g2,b2 = c in
+    								colors.(i) <- (r1+.r2,g1+.g2,b1+.b2)) () img in
 	let avg_colors = Array.mapi 
 			(fun i a -> let r,g,b = a and n = float_of_int (D.size u i) in
-    	(r/.n,g/.n,b/.n))	a in
-	I.map (fun x y c -> avg_colors.(D.find u (w*y+x))) img
+    	(r/.n,g/.n,b/.n))	colors in
+	let new_img = I.map (fun x y c -> avg_colors.(D.find u (w*y+x))) img in
+  I.map (fun x y c -> let n = D.find u (w*y+x) in
+           let up = D.find u (if y<h-1 then (w*(y+1)+x) else n)
+           and dn = D.find u (if y>0 then (w*(y-1)+x) else n)
+           and rt = D.find u (if x<w-1 then (w*y+x+1) else n)
+           and lt = D.find u (if x>0 then (w*y+x-1) else n)
+           and dr = D.find u (if y>0 && x<w-1 then (w*(y-1)+x+1) else n)
+           and dl = D.find u (if y>0 && x>0 then (w*(y-1)+x-1) else n)
+           and ur = D.find u (if y<h-1 && x<w-1 then (w*(y+1)+x+1) else n)
+           and ul = D.find u (if y<h-1 && x>0 then (w*(y+1)+x-1) else n) in
+    if n != up || n != dn || n != rt || n != lt || 
+       n != dr || n != dl || n != ur || n != ul then (255.0,255.0,255.0) 
+    else c) new_img
     
-let components ((n, _) : graph) (s : segmentation) : int list list =
-  let comp = Array.make n [] in
-  let rec helper n = 
-    if n >= 0 then 
-      let i = D.find s (n) in
-			comp.(i) <- ((n+1)::(comp.(i)));
-      helper n-1
-    else 
-			List.fold_left
-				(fun l x -> if x != [] then x::l else l) [] (Array.to_list comp)
-  in helper (n-1) []
-  
 (* Segments the image stored in the bitmap file located at img_filename
  * with the given parameters. Saves and shows the results *)
 let segmentAndShow (img_filename : string) (sigma : float)
       (k : float) (min_size : int) : unit =
   let img = I.load img_filename in
   let seg = segmentImage img sigma k min_size in
+  let _ = print_int (List.length (components (154401,[]) seg)) in
   let colored = colorComponents img seg in
     I.save colored "output/segmented.bmp";
     I.show colored
+
+let _ = segmentAndShow "images/beach_original.bmp" 0.5 500. 50
